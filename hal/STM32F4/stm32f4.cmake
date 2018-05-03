836ffd8598b819b86dd1f840ed5ec6a4d0fe7dcf
Sven Rademakers <sven.rademakers@gmail.com>
2018-05-03 07:12:38 +0200

    random changes. making the whole work

--- hal/STM32F4/stm32f4.cmake
+++ hal/STM32F4/stm32f4.cmake
@@ -1,6 +1,5 @@
 add_library(STM32F4_platform
 	${CMAKE_CURRENT_LIST_DIR}/startup.S
-	${CMAKE_CURRENT_LIST_DIR}/main.ld
+	${CMAKE_CURRENT_LIST_DIR}/stm32f4.ld
 	${CMAKE_CURRENT_LIST_DIR}/isr_vector.c
-	${CMAKE_CURRENT_LIST_DIR}/low_level_init.c
-	${CMAKE_CURRENT_LIST_DIR}/uart.c)
+	${CMAKE_CURRENT_LIST_DIR}/low_level_init.c)
---
+++ scripts/StartDebugger.bat
@@ -0,0 +1,3 @@
+
+start C:\Ac6\SystemWorkbench\plugins\fr.ac6.mcu.externaltools.openocd.win32_1.17.0.201801121207\tools\openocd\bin\openocd.exe -s C:\Ac6\SystemWorkbench\plugins\fr.ac6.mcu.debug_2.1.4.201801121207\resources\openocd\st_scripts -f openocd.cfg
+arm-none-eabi-gdb.exe
